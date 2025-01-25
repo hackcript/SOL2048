dfefd9a97eac5a11a971367579f28f7d2320ca58
hackcript <santygzzr@gmail.com>
2025-01-25 14:24:58 -0600

    Updated2

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -62,8 +62,7 @@ KeyboardInputManager.prototype.listen = function () {
   showInfo.addEventListener("touchend", this.showInfo.bind(this));
   
   var showKey = document.querySelector(".key-container");
-  showKey.addEventListener("click", this.showKey.bind(this));
-  showKey.addEventListener("touchend", this.showKey.bind(this));
+
   
   // var hideInfo = document.querySelector(".hide-info");
   // hideInfo.addEventListener("click", this.hideInfo.bind(this));
